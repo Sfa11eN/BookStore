cb5507f92142f42efb33c0fc3038d7bf8344f7d9
Саноцкий Александр <sfallenn@gmail.com>
2024-11-27 10:56:28 +0300

    Final commit

--- src/main/java/com/example/onlinebookstore/BookService.java
+++ src/main/java/com/example/onlinebookstore/BookService.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 public interface BookService {
     void addBook(Book book);
+
     Book getBookById(int id);
+
     List<Book> getAllBooks();
+
     void updateBook(Book book);
+
     void deleteBook(int id);
 }
-
-
-
-/**
- * Интерфейс для выполнения CRUD операций над книгами.
- */
